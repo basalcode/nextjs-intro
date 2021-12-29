bc3f8d6870be73dd34278bd26479fa4fe032b21a
basalcode <basalcode@gmail.com>
2021-12-29 20:21:47 +0900

    Create nextjs common pattern

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import Head from "next/head";
+import Seo from "./Seo";
+
 export default function Home() {
   return (
     <div>
+      <Seo title="Home" />
       <h1 className="active">Hello</h1>
     </div>
   );
