cc5251858691f4de44a107f2ad5eaf79b0f57fc6
basalcode <basalcode@gmail.com>
2021-12-30 21:58:25 +0900

    Create Api connection

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,30 @@
-import Head from "next/head";
+import { useEffect, useState } from "react";
+
 import Seo from "./Seo";
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { result } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=API_KEY`
+        )
+      ).json();
+      setMovies(result);
+    })();
+  }, []);
+
   return (
     <div>
       <Seo title="Home" />
-      <h1 className="active">Hello</h1>
+      {!movies && <h4>Lodaing...</h4>}
+      {movies?.map(movie => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
