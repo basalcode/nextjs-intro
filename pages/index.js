a46463c947c5e9923994ce6648db1f20f2cb732b
basalcode <basalcode@gmail.com>
2022-01-02 11:12:36 +0900

    Create movie detail page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,44 @@
-import { useEffect, useState } from "react";
-
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Seo from "./Seo";
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
+  }
+
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map(movie => (
-        <div className="movie" key={movie.id}>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <h4>
+            <Link
+              href={{
+                pathname: `/movie/${movie.id}`,
+                query: {
+                  title: movie.original_title,
+                },
+              }}
+              as={`/movie/$movie.id`}
+            >
+              <a>{movie.origianl_title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
@@ -18,6 +47,9 @@ export default function Home({ results }) {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
